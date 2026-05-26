8ba30601b2ca369028ba60908bce72c25a819c9e
seiji8 <sanfaedloni@students.unnes.ac.id>
2026-05-26 10:47:09 +0700

    f

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Dashboard')
 
 @section('page_css')
-    <link rel="stylesheet" href="{{ asset('css/pages/dashboard.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/pages/dashboard.css') }}?v={{ time() }}">
 @endsection
 
 @section('head')
